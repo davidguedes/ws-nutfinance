b3d642d7e838c643a787e9fc799515f20c4dcabb
David Guedes <d2vid.guedes@gmail.com>
2024-10-02 12:12:57 -0300

    Update FixedCron.ts
    teste de cron - fixas

--- src/jobs/FixedCron.ts
+++ src/jobs/FixedCron.ts
@@ -55,7 +55,8 @@ async function processFixedEntries() {
 
 // Cron job para rodar todos os dias à 00:01
 //const job = module.exports = cron.schedule('*/1 * * * *', processFixedEntries, {
-const job = module.exports = cron.schedule('1 0 * * *', processFixedEntries, {
+//const job = module.exports = cron.schedule('1 0 * * *', processFixedEntries, {
+const job = module.exports = cron.schedule('15 12 * * *', processFixedEntries, {
   scheduled: false,
   timezone: 'America/Sao_Paulo'
 });
